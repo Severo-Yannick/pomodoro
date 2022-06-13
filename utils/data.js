5d36b4a9f8e7ffbdaf9bbb1ac5e063e8fc548148
Severo-Yannick <severoyannick@gmail.com>
2022-06-13 18:39:31 +0200

    data

---
+++ utils/data.js
@@ -0,0 +1,17 @@
+export const footerNavigation = [
+  {
+    id: 1,
+    label: 'Timer',
+    icon: 'timer'
+  },
+  {
+    id: 2,
+    label: 'Settings',
+    icon: 'settings'
+  },
+  {
+    id: 3,
+    label: 'History',
+    icon: 'history'
+  }
+]
