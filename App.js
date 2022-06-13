71bc2817b63739a889a04ab4f48feb0b5d89fa23
Severo-Yannick <severoyannick@gmail.com>
2022-06-13 18:39:11 +0200

    navigation components

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Timer from './components/Timer/Timer'
+import History from './components/History.jsx/History'
+import Settings from './components/Settings/Settings'
+import Footer from './components/Footer/Footer'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Timer'>
+          <Stack.Screen name='Timer' component={Timer} />
+          <Stack.Screen name='History' component={History} />
+          <Stack.Screen name='Settings' component={Settings} />
+        </Stack.Navigator>
+        <Footer />
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
@@ -15,6 +31,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
